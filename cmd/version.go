37e43b8024ba30ac2ba8e5422ab99a747c3616b6
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-05-23 14:28:54 -0500

    feat(version.go): add the new version command with aliases and examples :D

--- cmd/version.go
+++ cmd/version.go
@@ -16,36 +16,27 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/Moldy-Community/CLI/utils"
 	"github.com/spf13/cobra"
 )
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Show the version of moldy :)",
+	Long: `Show the version of Moldy:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+[In error case]
+
+This show the current version of moldy any error
+Report on Github :D`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		utils.Info("Moldy 0.0.1 Alpha")
 	},
+	Aliases: []string{"v", "ver"},
+	Example: "moldy version",
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
