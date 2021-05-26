45c57079c6a0feb25d0311cdf35fe982fe1086b4
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-05-26 11:45:28 -0500

    refactor(envfile.go): change the get env function
    chage the function for a multiple usage :p

--- utils/envfile.go
+++ utils/envfile.go
@@ -6,9 +6,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetDotEnv() string {
+func GetDotEnv(key string) string {
 	err := godotenv.Load()
-	CheckErrors(err, "code 2", "Error reading the enviroment variables", "Report the error in the Github :D")
+	CheckErrors(err, "code 2", "Error reading the enviroment variables", "Report the error in the Github :p")
 
-	return os.Getenv("CRYPTO_KEY")
+	return os.Getenv(key)
 }
