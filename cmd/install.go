831d9e17326e53e5668c2a17f556fb07fae78bcb
Skim <84688663+GuillermoMajano@users.noreply.github.com>
2021-06-01 15:12:23 -0400

    Create install.go
    a package for install external package ("builder")

---
+++ cmd/install.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	_"github.com/Moldy-Community/moldy/core/config"
+	"github.com/Moldy-Community/moldy/utils/function/packages"
+	"github.com/spf13/cobra"
+)
+
+
+var createInstall bool
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Search packages",
+	Long: `:
+        allow searching and extracting the content of a github packages on github
+       `,
+	Run: func(cmd *cobra.Command, args []string) {
+		if createInstall {
+			if flags,_:= cmd.Flags().GetBool("url"), flags{
+				//do something
+			}else{
+				  //packages.GetId().go
+				}
+		   }
+	      },
+	Example: "moldy i name-of-pkg or moldy install --url github.com/user/name",
+	Aliases: []string{"i", "ins"},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+	configCmd.Flags().BoolP(&createInstall, "url", false, "a command for download package")
+}
+
