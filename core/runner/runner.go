2039fa0700124879c640cb9821d72f2f65567e36
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-06-02 18:59:42 -0500

    feat(runner.go): add the new code only need execute :D

--- core/runner/runner.go
+++ core/runner/runner.go
@@ -2,17 +2,36 @@ package runner
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Moldy-Community/moldy/utils/colors"
-	"github.com/spf13/viper"
+	"github.com/Moldy-Community/moldy/utils/functions"
+	toml "github.com/pelletier/go-toml"
 )
 
-func RunnerWorker() {
-	runVal := viper.GetStringMapString("moldyRunner")
-	for n, c := range runVal {
-		colors.Info("Name Of the command: \n")
-		fmt.Println(n)
-		colors.Info("Value of the command: \n")
-		fmt.Println(c)
+func RunnerWorker(name string) {
+	if _, err := os.Stat("./MoldyFile.toml"); err == nil {
+		file, err := toml.LoadFile("MoldyFile.toml")
+		functions.CheckErrors(err, "Code 2", "Error in read the MoldyFile toml con run the command", "Check the moldy or user permissions")
+		keysCmd := file.Get("moldyrunner").(*toml.Tree).Keys()
+		for _, k := range keysCmd {
+			if k == name {
+				colors.Info("Command name to run:")
+				fmt.Println(k)
+
+				val := file.Get("moldyrunner").(*toml.Tree).Get(k).(string)
+
+				colors.Info("Command value to run:")
+				fmt.Println(val)
+				break
+			} else {
+				colors.Error("Error command not found exit :(")
+			}
+		}
+
+	} else if os.IsNotExist(err) {
+		colors.Error("Moldy file not exist run **moldy config -g** for generate a config")
+	} else {
+		colors.Error("WTF How you are here? :o")
 	}
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/Moldy-Community/moldy
 go 1.16
 
 require (
+	github.com/ahmetalpbalkan/go-dexec v0.0.0-20160630173852-2538e5e2198f // indirect
 	github.com/fatih/color v1.12.0
+	github.com/fsouza/go-dockerclient v1.7.2 // indirect
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/joho/godotenv v1.3.0
 	github.com/manifoldco/promptui v0.8.0
 	github.com/matthewhartstonge/argon2 v0.1.4
 	github.com/olekukonko/tablewriter v0.0.5
+	github.com/pelletier/go-toml v1.2.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.4.0 // indirect
