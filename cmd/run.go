dd56e8a41047845e5de4075ab063ad2ee4f283c4
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-07-14 16:27:02 -0500

    refractor(run.go): Change the description and add FAQ

--- cmd/run.go
+++ cmd/run.go
@@ -24,28 +24,19 @@ import (
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "You need automate the scripts for a faster develop here! Moldy Scripts",
+	Long: `For the people like the automate your development! Moldy Scripts
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+FAQ:
+Works in all platforms?: Yeah this is a multiplatform tool as Moldy
+
+No more questions? Yes, Moldy scripts is simply awesome any question you can open a issue.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		runner.RunnerWorker(args[0])
 	},
+	Example: "moldy run setup",
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
