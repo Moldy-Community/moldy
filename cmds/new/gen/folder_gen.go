37a82c303bc16e41a2c05fbcb423544cc9bce430
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-12-19 16:20:53 -0500

    style(format): check the files with fumpt

--- cmds/new/gen/folder_gen.go
+++ cmds/new/gen/folder_gen.go
@@ -3,15 +3,15 @@ package gen
 import (
 	"os"
 
+	log "github.com/Moldy-Community/moldy/core/logger"
 	"github.com/Moldy-Community/moldy/utils/errors"
-  log 	"github.com/Moldy-Community/moldy/core/logger"
 )
 
 func GenerateFolders(folders []string) {
 	for _, f := range folders {
-		err := os.Mkdir(f, 0755)
-    errors.CheckError(err, "Code 2", "Error in create the folders", "Check the permissions, or re run with the sudo or admin permissions or report the error on github")
+		err := os.Mkdir(f, 0o755)
+		errors.CheckError(err, "Code 2", "Error in create the folders", "Check the permissions, or re run with the sudo or admin permissions or report the error on github")
 	}
 
-  log.Logger.Info("Succesfully created the folders")
+	log.Logger.Info("Succesfully created the folders")
 }
