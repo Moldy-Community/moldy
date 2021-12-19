7904e5ac12b71eb81144edfe0a3f6211b69c34bd
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-12-19 16:11:18 -0500

    feat: start the generation of the file and the extra files data

---
+++ cmds/new/extra_files.go
@@ -0,0 +1,60 @@
+package new
+
+var EditorConfigData = []byte(`
+root = true
+[*]
+end_of_line = lf
+insert_final_newline = true
+indent_style = space
+charset = utf-8
+trim_trailing_whitespace = true
+indent_size = 2
+##################################
+####### Made by Moldy CLI ########
+##################################
+`)
+
+var GitIgnoreData = []byte(`
+# Ignoring the .env files
+
+*.env
+
+# Editor ignore folders
+*.idea/
+*.vscode/
+*.vim/
+
+##################################
+####### Made by Moldy CLI ########
+##################################
+`)
+
+var ReadmeData = []byte(`
+# Example package
+
+Example content and example description :p
+
+# Credits
+
+**Author:** Jhon Doe
+**Version:** v1.0.0
+**Url:** [here](https://example.com)
+
+> Made with :heart: by Moldy CLI [here](https://github.com/Moldy-Community/moldy)
+`)
+
+var LicenseData = []byte(`
+
+Copyright © 2021 Example Author
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`)
---
+++ cmds/new/moldy_gen.go
@@ -0,0 +1,50 @@
+package new
+
+import (
+	"os"
+
+	"github.com/Moldy-Community/moldy/utils/errors"
+	vp "github.com/spf13/viper"
+)
+
+/* Add the default values, paths, aliases and config name and type */
+var (
+	defaults = map[string]interface{}{
+		"adminProjects": map[string]bool{
+			"git":                 true,
+			"conventionalCommits": true,
+			"changelog":           true,
+		},
+		"aparienceOptions": map[string]bool{
+			"progressBar": true,
+			"logColors":   true,
+		},
+		"scripts": map[string]string{
+			"test": "echo 'Running a example command'",
+		},
+	}
+	paths = []string{
+		"./",
+	}
+	configName = "MoldyFile.toml"
+	configType = "toml"
+)
+
+/* Creating the config file */
+func NewMoldyFile() {
+	for _, p := range paths {
+		vp.AddConfigPath(p)
+	}
+	vp.SetConfigName(configName)
+	vp.SetConfigType(configType)
+	for k, v := range defaults {
+		vp.SetDefault(k, v)
+	}
+
+	if err := vp.SafeWriteConfigAs(configName); err != nil {
+		if os.IsNotExist(err) {
+			err = vp.WriteConfigAs(configName)
+			errors.CheckError(err, "Code 2", "Error in write the config file", "Report the error on github or re try the command with new permmisions")
+		}
+	}
+}
